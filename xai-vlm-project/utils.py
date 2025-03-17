0cce566d8d0eb81fe7a1567ed3b53911f89cd348
pikko5 <heldrive07@gmail.com>
2025-03-18 02:49:34 +0700

    initial commit

---
+++ xai-vlm-project/utils.py
@@ -0,0 +1,35 @@
+import numpy as np
+from scipy.ndimage import filters
+import matplotlib.pyplot as plt
+from PIL import Image
+def normalize(x: np.ndarray) -> np.ndarray:
+    # Normalize to [0, 1].
+    x = x - x.min()
+    if x.max() > 0:
+        x = x / x.max()
+    return x
+
+# Modified from: https://github.com/salesforce/ALBEF/blob/main/visualization.ipynb
+def getAttMap(img, attn_map, blur=True):
+    if blur:
+        attn_map = filters.gaussian_filter(attn_map, 0.02*max(img.shape[:2]))
+    attn_map = normalize(attn_map)
+    cmap = plt.get_cmap('jet')
+    attn_map_c = np.delete(cmap(attn_map), 3, 2)
+    attn_map = 1*(1-attn_map**0.7).reshape(attn_map.shape + (1,))*img + \
+            (attn_map**0.7).reshape(attn_map.shape+(1,)) * attn_map_c
+    return attn_map
+
+def viz_attn(img, attn_map, blur=True):
+    _, axes = plt.subplots(1, 2, figsize=(10, 5))
+    axes[0].imshow(img)
+    axes[1].imshow(getAttMap(img, attn_map, blur))
+    for ax in axes:
+        ax.axis("off")
+    plt.show()
+    
+def load_image(img_path, resize=None):
+    image = Image.open(img_path).convert("RGB")
+    if resize is not None:
+        image = image.resize((resize, resize))
+    return np.asarray(image).astype(np.float32) / 255.
